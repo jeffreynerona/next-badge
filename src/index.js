0164d47be432dd820ce6aedcd07713e44f405178
jeffreynerona <jeffnerona@gmail.com>
2018-02-19 15:46:25 +0800

    Fixed qr generation on socket

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const LocalStrategy = require('passport-local').Strategy;
 
 import config from './config';
 import routes from './routes';
+import Host from './model/host';
 
 var RateLimit = require('express-rate-limit');
 
@@ -121,11 +122,76 @@ io.on('connection', (client) => {
   var qrgenerator;
   client.on('hostEvent', (event) => {
     console.log(client.id+'-Hosting '+event.eventid+'. Interval: '+event.interval+' seconds');
-    var x = 0;
+    var qrdetails;
     qrgenerator = setInterval(() => {
-      x+=1;
-      client.emit('qrcode', x);
-    }, event.interval);
+    Host.find({
+      "event" : event.eventid,
+      "endtime" : { $exists: false }
+    }, (err, hosts) => {
+      if (err) {
+        qrdetails = {
+          success: false,
+          message: 'There was an error with the server. Query Hosts Error. Please contact the administrator.',
+          code: ''
+        };
+      } else {
+        console.log('search host success');
+        if(hosts.length>=1) {
+          //found an active host
+          //generate qr shit and connect the sockiets(happens in client)
+          var toupdate = hosts[hosts.length-1];
+          var qrupdated = Date.now().toString();
+          var qr = md5(event.id.toString() + qrupdated);
+          qr = qr.substr(qr.length - 5);
+          toupdate.save(err => {
+            if (err) {
+              qrdetails = {
+                success: false,
+                message: 'There was an error with the server. Update Host Error. Please contact the administrator',
+                code: ''
+              };
+            } else {
+              qrdetails = {
+                success: true,
+                message: 'QR code successfully regenerated.',
+                code: qr
+              };
+            }
+          });
+        }
+        else {
+          //no active host, create one
+          var now = new Date();
+          var end = now;
+          var qrupdated = Date.now().toString();
+          var qr = md5(event.id.toString() + qrupdated);
+          qr = qr.substr(qr.length - 5);
+          // end.setHours(end.getHours() + 12);
+          let newHost = new Host();
+          newHost.event = event.id;
+          newHost.host = event.owner;
+          newHost.starttime = now.toISOString();
+          newHost.qr = qr;
+          newHost.save(err => {
+            if (err) {
+              qrdetails = {
+                success: false,
+                message: 'Error in creating Host Server',
+                code: ''
+              };
+            } else {
+              qrdetails = {
+                success: false,
+                message: 'Successfully created host',
+                code: newHost.qr
+              };
+            }
+          });
+        }
+      }
+    });
+      client.emit('qrcode', qrcode);
+    }, 10000);
   });
   client.on('disconnect', function(){
       console.log( client.id + ' has disconnected');
